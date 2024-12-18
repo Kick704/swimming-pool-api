a5d3df36c2cf599c19f30386119065bac1a647e3
Kick704 <xajretdinov.dinis@mail.ru>
2024-12-18 15:33:25 +0500

    Initial commit

---
+++ src/main/java/com/swimming_pool/management/SwimmingPoolManagementRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.swimming_pool.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimmingPoolManagementRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimmingPoolManagementRestApiApplication.class, args);
+	}
+
+}
